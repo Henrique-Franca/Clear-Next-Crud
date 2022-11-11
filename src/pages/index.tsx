d05bf3d92494775bf1fc53d66480ba0fa646ac74
Henrique França <henriqsf@gmail.com>
2022-11-11 09:37:03 -0300

    Componente Tabela #01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes =[
+    new Cliente('Ana',   34, '1'),
+    new Cliente('Bia',   25, '2'),
+    new Cliente('Carlos',54, '3'),
+    new Cliente('Pedro', 32, '4'),
+  ]
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -9,7 +19,7 @@ export default function Home() {
     `}>
         <Layout titulo="Cadastro Simples">
 
-          <span>Conteudo...</span>
+          <Tabela cliente={clientes}></Tabela>
 
         </Layout>
     </div>
