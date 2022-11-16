722c67bb3d427bc2a6a1469ee6cc0d4f9be6acdc
Henrique França <94202846+Henrique-Franca@users.noreply.github.com>
2022-11-16 10:16:46 -0300

    Integrando Tabela e Formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import Cliente from "../core/Cliente";
 
 export default function Home() {
 
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
   const clientes =[
     new Cliente('Ana',   34, '1'),
     new Cliente('Bia',   25, '2'),
@@ -15,20 +18,26 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente: Cliente){
-    console.log(cliente.nome)
+    setCliente(cliente)
+    setVisivel('form')
   }
   
   function clienteExcluido(cliente: Cliente){
     console.log(`Excluir... ${cliente.nome}`)
   }
 
-  function salvarCliente(cliente:Cliente){
-
-    console.log(cliente)
+  function novoCliente(){
+    setCliente(Cliente.vazio())
+    setVisivel('form')
 
   }
 
-  const [visivel, setVisivel] =useState<'tabela' | 'form'>('tabela')
+  function salvarCliente(cliente:Cliente){
+
+    console.log(cliente)
+    setVisivel('tabela')
+
+  }
 
   return (
     <div className={`
@@ -40,19 +49,20 @@ export default function Home() {
           {visivel === 'tabela' ? (
             <>
               <div className="flex justify-end">
-                <Botao cor="green" className="mb-4" onClick={() => setVisivel('form')}>
+                <Botao cor="green" className="mb-4" onClick={novoCliente}>
                   Novo Cliente
                 </Botao>
               </div>
 
               <Tabela cliente={clientes} 
-                clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}/>
+                clienteSelecionado={clienteSelecionado} 
+                clienteExcluido={clienteExcluido}/>
             </>
 
           ): (
 
             <Formulario 
-            cliente={clientes[1]} 
+            cliente={cliente} 
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}
             />
